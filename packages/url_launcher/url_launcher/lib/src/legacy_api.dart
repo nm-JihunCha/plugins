a58c37d19248531f1b41b29123b8814ffe4168f3
차지훈[넷마블] <t2203530@partner.coway.com>
2023-01-02 18:55:04 +0900

    feat: lint fix

--- packages/url_launcher/url_launcher/lib/src/legacy_api.dart
+++ packages/url_launcher/url_launcher/lib/src/legacy_api.dart
@@ -130,8 +130,6 @@ Future<bool> launch(
 /// details.
 @Deprecated('Use canLaunchUrl instead')
 Future<bool> canLaunch(String urlString) async {
-  print('canLaunch - $urlString');
-
   return UrlLauncherPlatform.instance.canLaunch(urlString);
 }
 
